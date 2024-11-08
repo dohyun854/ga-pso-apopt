cbedab661026b42c05abd6a0743da02e668ce7d4
Marongstars <marongstars@gmail.com>
2024-11-08 12:59:44 +0900

    Add image.py
    2024.11.08

--- image.py
+++ image.py
@@ -2,22 +2,21 @@ import cv2
 import numpy as np
 
 def extract_all_wall_coordinates(image_path):
-    # 1. 이미지 로드
+
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     
-    # 2. 이진화 (Thresholding)
+    
     _, binary_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
 
-    # 3. 벽 픽셀 좌표 추출
+
     wall_coordinates = []
     for y in range(binary_image.shape[0]):
         for x in range(binary_image.shape[1]):
-            if binary_image[y, x] == 255:  # 흰색 픽셀일 경우
+            if binary_image[y, x] == 255:  
                 wall_coordinates.append((x, y))
     
     return wall_coordinates
 
-# 예시 사용
 image_path = 'path_to_your_image.png'
 wall_coordinates = extract_all_wall_coordinates(image_path)
-print("벽의 모든 픽셀 좌표:", wall_coordinates)
+print(wall_coordinates)
